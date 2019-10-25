f9274814635d49b12690c2ba64e90599341fc10c
harshit bhalla <30543655+harshitsan@users.noreply.github.com>
2019-10-25 06:43:33 +0530

    Create coin.cpp

---
+++ coin.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+ll numberofways(int n, vector <int> &coins, int value) {
+
+	vector <ll > ways(value + 1);
+	ways[0] = 1;
+
+	for(auto coin : coins) {
+		for(int i = 1; i < value + 1; i++) {
+			if(i >= coin) {
+				ways[i] += ways[i - coin];
+			}
+		}
+	}
+	return ways[value];
+
+}
+
+void solve() {
+	int n;//number of coins
+	cin >> n;
+	vector <int > coins(n); //values of the coins
+
+	for(int i = 0; i < n; i++) 
+		cin >> coins[i];
+
+	int value; // required value;
+	cin >> value;
+
+	cout << numberofways(n, coins, value) << endl;
+
+}
+
+int main() {
+	int t;//number of testcases
+	cin >> t;
+	while(t--) {
+		solve();
+	}
+	return 0;
+}
